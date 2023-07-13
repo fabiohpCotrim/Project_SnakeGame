8bc2aa12a07037f11daa5b024c55b7615d216800
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-07-13 12:11:11 -0300

    GamePanel skeleton

--- src/application/GamePanel.java
+++ src/application/GamePanel.java
@@ -1,20 +1,57 @@
 package application;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
 	public GamePanel() {
-
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 	}
 
 }
