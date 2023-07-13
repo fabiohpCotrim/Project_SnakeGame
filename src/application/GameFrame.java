14e657e24ae9687c2694e47738584f2ccd0fcbeb
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-07-13 15:03:53 -0300

    Pause and resume methods

--- src/application/GameFrame.java
+++ src/application/GameFrame.java
@@ -3,6 +3,7 @@ package application;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
 
 	public GameFrame() {
 		this.add(new GamePanel());
--- src/application/GamePanel.java
+++ src/application/GamePanel.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
 
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
@@ -41,6 +42,18 @@ public class GamePanel extends JPanel implements ActionListener {
 		timer.start();
 	}
 
+	public void pauseGame() {
+		timer.stop();
+	}
+
+	public void resumeGame() {
+		timer.start();
+	}
+
+	public void setHighScore() {
+
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
@@ -49,11 +62,10 @@ public class GamePanel extends JPanel implements ActionListener {
 	public void draw(Graphics g) {
 		if (running) {
 			/*
-			for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-				g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-				g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-			}
-			*/
+			 * for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) { g.drawLine(i *
+			 * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT); g.drawLine(0, i * UNIT_SIZE,
+			 * SCREEN_WIDTH, i * UNIT_SIZE); }
+			 */
 			g.setColor(Color.red);
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
@@ -62,17 +74,17 @@ public class GamePanel extends JPanel implements ActionListener {
 					g.setColor(Color.green);
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 				} else {
-					//g.setColor(new Color(45, 180, 0));
+					// g.setColor(new Color(45, 180, 0));
 					g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 				}
 			}
 			g.setColor(Color.white);
-			g.setFont(new Font("Ink Free",Font.BOLD, 40));
+			g.setFont(new Font("Ink Free", Font.BOLD, 40));
 			FontMetrics metrics = getFontMetrics(g.getFont());
-			g.drawString("Score: "+ applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+ applesEaten))/2, g.getFont().getSize());
-		}
-		else {
+			g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2,
+					g.getFont().getSize());
+		} else {
 			gameOver(g);
 		}
 	}
@@ -141,16 +153,21 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 
 	public void gameOver(Graphics g) {
-		//Score
+		// Score
 		g.setColor(Color.white);
-		g.setFont(new Font("Ink Free",Font.BOLD, 40));
+		g.setFont(new Font("Ink Free", Font.BOLD, 40));
 		FontMetrics metrics1 = getFontMetrics(g.getFont());
-		g.drawString("Score: "+ applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+ applesEaten))/2, g.getFont().getSize());
-		//Game Over text
+		g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: " + applesEaten)) / 2,
+				g.getFont().getSize());
+		// Game Over text
 		g.setColor(Color.red);
-		g.setFont(new Font("Ink Free",Font.BOLD, 75));
+		g.setFont(new Font("Ink Free", Font.BOLD, 75));
 		FontMetrics metrics2 = getFontMetrics(g.getFont());
-		g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+
+		if (!running) {
+
+		}
 
 	}
 
@@ -189,6 +206,12 @@ public class GamePanel extends JPanel implements ActionListener {
 				if (direction != 'U') {
 					direction = 'D';
 				}
+				break;
+			case KeyEvent.VK_ESCAPE:
+				pauseGame();
+				break;
+			case KeyEvent.VK_SPACE:
+				resumeGame();
 				break;
 			}
 
